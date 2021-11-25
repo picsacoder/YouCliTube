cc359a7e2e9aa32dfb38968a9b64aa66d5d4a530
PcsaDev <omarsaidortiz09@gmail.com>
2021-11-24 18:13:10 -0600

    asdada

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const ytdl = require('ytdl-core');
+var ProgressBar = require('progress');
 const fs = require('fs')
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-const colors = require('colors')
+const chalk = require('chalk')
 
+async function main(url) { 
+    
+    let info = await ytdl.getInfo(url);
 
-readline.question('Please put the URL here:', url => {
     ytdl(url)
-        .pipe(fs.createWriteStream('video.mp4'));
-    readline.close()
-});
+        .pipe(fs.createWriteStream(`${info.videoDetails.title}.mp4`));
+
+    var bar = new ProgressBar(`:bar ${chalk.red(':current')}${chalk.grey('/')}${chalk.blue(':total')}`, { total: 10 });
+    var timer = setInterval(function () {
+        bar.tick();
+        if (bar.complete) {
+        console.log(`\n${chalk.greenBright('Video downloaded')}\n`);
+        clearInterval(timer);
+        }
+    }, 100);
+
+
+}
+
+main('https://www.youtube.com/watch?v=UT5F9AXjwhg')
